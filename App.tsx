9cb6782fb8bea1f34411ad9865510f6c8f5a340e
Cássio Marcião <marciaocassio@gmail.com>
2023-06-18 17:19:36 -0300

    feat: Add Text component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from './src/components/Text';
 
 export default function App() {
+    const [isFontsLoaded] = useFonts({
+        'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+        'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+        'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    });
+
+    if(!isFontsLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title} >Waiter Mobile App!</Text>
+            <Text>Waiter Mobile App!</Text>
             <StatusBar style="auto" />
         </View>
     );
@@ -16,9 +28,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
-    },
-    title: {
-        fontWeight: '500',
-        fontSize: 24
     }
 });
