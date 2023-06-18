9bfec9805cef9a063671f62ac6c25b4f2bf8f73f
Cássio Marcião <marciaocassio@gmail.com>
2023-06-18 16:32:02 -0300

    style: Apply eslint rules

--- App.tsx
+++ App.tsx
@@ -2,23 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title} >Waiter Mobile App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title} >Waiter Mobile App!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontWeight: '500',
-    fontSize: 24
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: '500',
+        fontSize: 24
+    }
 });
