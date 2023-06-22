ea1e9f107c14fabec6eeb344fa8afbd16ab2b412
Cássio Marcião <marciaocassio@gmail.com>
2023-06-21 21:59:17 -0300

    feat: Update table modal component

--- src/components/TableModal/index.tsx
+++ src/components/TableModal/index.tsx
@@ -22,6 +22,7 @@ export function TableModal({ isVisible, onClose, onSave }: TableModalProps) {
     const [table, setTable] = useState('');
 
     function handleSave() {
+        setTable('');
         onSave(table);
         onClose();
     }
